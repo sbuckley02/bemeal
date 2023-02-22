578a26f003f425ad3f347963939dafefdf696b97
Ethan Li <ethanli@lawn-143-215-98-77.lawn.gatech.edu>
2023-02-21 19:19:00 -0500

    Updated the main screen

--- bemeal-client/src/components/Home.js
+++ bemeal-client/src/components/Home.js
@@ -1,4 +1,5 @@
 // Imports
+import logo from '/Users/ethanli/bemeal/bemeal-client/src/bemeal logo.png';
 import axios from 'axios';
 import {useState, useEffect} from 'react';
 
@@ -22,13 +23,17 @@ function Home() {
  	// embedded variable
 	return (
 		<div>
-			<h2>BeMeal</h2>
-			<p>This text can be found in the nested component "Home". You're gonna want to change this text when you're able to</p>
+			<h2>Welcome to BeMeal!</h2>
+			<h3>In this application, you will be able to take photos of your meals 
+				3 times a day to show off to your friends, and see what your friends ate as well!</h3>
+			<img src={logo} className = "logo" alt="" />
+			{/* <p>This text can be found in the nested component "Home". You're gonna want to change this text when you're able to</p>
 			<p>Here's something cool - the following text is retreived from the back-end:</p>
 			<code>{sampleBackendText}</code>
 			<p>If you see <code>null</code> above, it means it didn't work. That likely means that you don't have the
- 				back-end running on a separate port on your computer. If you see something else, it's working</p>
+ 				back-end running on a separate port on your computer. If you see something else, it's working</p> */}
 		</div>
+		
 	)
 }
 
